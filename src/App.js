a71bd3bb86cdad5a87113932c69ca0e6c6897a77
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2022-01-11 21:34:26 +0200

    finishing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect, useRef } from 'react'
 
 function App() {
+
+  const [counter,setCounter]=useState(0)
+  const [name, setName] = useState("")
+
+  useEffect (()=>{
+    console.log('counter updated')
+    //setCounter(counter +1)
+    //alert('welcome to our app')
+  },[counter])
+
+  const firstMount = useRef(true)
+
+  useEffect(()=>{
+    console.log( 'before',firstMount.current)
+    if (firstMount.current){
+      firstMount.current = false 
+      console.log( firstMount.current)
+    }else{
+      console.log('skip the mount but run any other update ')
+      
+    }  
+
+        
+  })
+
+
+
+
+
+
+
+  // useEffect (()=>{
+  //   console.log('name updated')
+  // },[name])
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{counter}</p>
+      <button onClick= {()=>{setCounter(counter +1 )}}>add</button>
+      <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+      <p>{name && `Hello ${name}`}</p>
+     
     </div>
   );
 }
